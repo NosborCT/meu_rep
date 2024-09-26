7c7dcd93b1e4c0b90af780c3ed4d695f88c121ab
NosborCT <hainiken08@gmail.com>
2024-09-26 17:39:58 -0300

    Fix: atualizando o arquivo da api adicioanando o token de uso do github.

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // link da api utilizada
-    baseURL:'https://api.github.com/',
+    baseURL: 'https://api.github.com',
+    headers: {
+        Authorization: 'Bearer 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'
+    }
 });
 
 export default api;
