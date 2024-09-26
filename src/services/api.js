c9cf91dd02dc37d62fcfc3411a544a8cbf190539
NosborCT <hainiken08@gmail.com>
2024-09-26 17:44:26 -0300

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://api.github.com',
-    headers: {
-        Authorization: 'Bearer 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'
-    }
+   
 });
 
 export default api;
